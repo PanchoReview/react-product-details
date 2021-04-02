6d9ce1e392f0847857b1c5d73140cf00240e364d
Francisco Astorga <fastorga@nimok.com>
2021-04-02 19:20:25 -0300

    Modified Navbar elements. Added Amazon logo.
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,10 @@ export default class NavBar extends  Component {
     render() {
         return(
             <div className={classes.NavBar}>
-                <h1 className={classes.NavBarTitle}>Amazon, bitch</h1>
+                <img className={classes.AmazonLogo}
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png" 
+                        alt="Amazon logo" />     
+                <p>not actually amazon</p>       
             </div>
         )
     }
